45b623156f0489e1d87632491b1420d7f34884e9
a00871293 <a00871293@SE12L0321-20.ad.bcit.ca>
2017-01-10 16:55:47 -0800

    hayden is a boss

--- index.php
+++ index.php
@@ -11,8 +11,32 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo 'Hello, out there.... I live';
+        include('student.php');
+        $students = array();
         
+        foreach($students as $student)
+            echo $student ->tostring();
+        
+        $first = new Student();
+        $first->surname = "Ng";
+        $first->first_name = "Hayden";
+        $first->add_email('home','haydenIsBoss@gmail.com');
+        $first->add_email('work','haydenBoss@boss.com');
+        $first->add_grade(98);
+        $first->add_grade(96);
+        $first->add_grade(100);
+        $students['h123'] = $first;
+        
+        $second = new Student();
+        $second->surname = "Wong";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(20);
+        $second->add_grade(30);
+        $second->add_grade(50);
+        $students['a456'] = $second;
         ?>
     </body>
 </html>
